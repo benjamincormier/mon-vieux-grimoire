51a12d69d3d9a89337eb3bcba1442ca396f4e133
Benjamin Cormier <benjamin.cormier@outlook.com>
2023-07-18 15:19:58 +0200

    Add getAllBooks in books controller and bug fix in app.js for static images

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,6 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]; // Bearer token
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    console.log('req.headers.authorization :', req.headers.authorization);
-    console.log('decodeToken :', decodedToken);
     const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
